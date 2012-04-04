7dbff124c26b7d525daf6f5ce7befcbc11003bf9
paulter <paul.terwil@gmail.com>
2012-04-04 20:39:57 -0300

    Both bugs caused by the same source: both bugs fixed.  Code cleaned up.

--- reversi.py
+++ reversi.py
@@ -2,10 +2,7 @@
 Paul Terwilliger, Jay Paik, Kirsten Lau"""
 import re
 
-"""The code in prompt_move() is still ugly and can use some cleaning up. 
-I'll probably do some things to make it run faster and look cleaner.  
-
-A few things that the rules still do not do: 
+"""A few things that the rules still do not do: 
 a)tell you when the game is over
 b)end the game
 c)force a player to pass if they cannot move
@@ -24,11 +21,7 @@ b)asking for a rematch with sides switched Ex: black moves first if white
     moved first last game and vice-versa
 c)resetting the board using the new_game() function
 
-Bug found: when the series of numbers 53 32 24 63 42 35 is input, 35 doesn't
-flip over all of the pebbles that it should, namely the ones to the west of
-it.  I don't know why it doesn't flip them.  If someone finds it, you are
-amazing. I'll look at it later, thanks!
-
+---BUG FIXED---
 (Jay) Question:
 Where would you like to move? Choose a two digit number 00-77: 21
 [(2, 2), (3, 2)] <---- These are the flipped pebbles
@@ -48,6 +41,7 @@ Where would you like to move? Choose a two digit number 00-77: 54
 Invalid entry, try again
 
 In this case, as Player 2, I cannot move to 54. Is this a bug?
+---BUG FIXED---
 """
 
 class Reversi(object):
@@ -72,6 +66,12 @@ class Reversi(object):
                       [0, 0, 0, 0, 0, 0, 0, 0,]]
         self.turn = ((self.turn % 2) + 1)
         self.print_board()
+
+    def end_game(self):
+        """Ends a reversi game. Should output who wins and ask for a rematch
+        (with sides switched). If a rematch is decided upon, new_game should
+        be run."""
+        pass
         
     def print_board(self):
         """Prints the board: temporary function for developing code"""
@@ -84,7 +84,8 @@ class Reversi(object):
 
     def flip_pebbles(self):
         """Checks if any opponent pebbles are around the input pebble and
-        stores the coordinates of all opposing flippable pebbles in flip"""
+        flips the corresponding pebbles.  Returns a list of flipped
+        pebbles.  If the move is illegal, the turn won't happen."""
         
         def legal(flip_list):
             """Input is a list of two-integer tuples which are coordinates,
@@ -92,7 +93,6 @@ class Reversi(object):
             legl = 0
             counter = 0
             for (num, ber) in flip_list:
-                #print (num, ber)
                 if self.board[num][ber] == ((self.turn % 2) + 1):
                     counter += 1
                 elif self.board[num][ber] == 0:
@@ -174,16 +174,20 @@ class Reversi(object):
                             self.board[coo_y - 1][coo_x - 1] == turn_f):
             coors_y = [coor for coor in range(coo_y - 1, -1, -1)]
             coors_x = [coor for coor in range(coo_x - 1, -1, -1)]
-            west = zip(coors_y, coors_x)
+            nwest = zip(coors_y, coors_x)
         
         self.flip = (legal(north) + legal(neast) + legal(east)
                    + legal(seast) + legal(south) + legal(swest)
                     + legal(west) + legal(nwest))
         
         # Flips over the pebbles chosen by flip
+        lgal = 0
         for (num, ber) in self.flip:
+            lgal += 1
             self.board[num][ber] = self.turn
-        print self.flip, "<---- These are the flipped pebbles"
+        if 0 < lgal:
+            self.board[int(self.move[0])][int(self.move[1])] = self.turn
+        print self.flip, "<---- Flipped pebbles on p{} turn".format(self.turn)
         return self.flip
         
     def prompt_move(self):
@@ -207,29 +211,37 @@ class Reversi(object):
         
         --Note: 1 is white, 2 is black, 0 is an empty square
         """
-        
+        # The prompt for a move
         self.move = '99'
-        while bool(re.match('[0-7][0-7]$', self.move)) == False:
+        while (bool(re.match(r'[0-7][0-7]$', self.move))) == False:
             self.move = raw_input("Where would you like to move? "
                                   "Choose a two digit number 00-77: ")
-            if (bool(re.match(r'[0-7][0-7]$', self.move)) == True) and (len(
-                                                    self.move) == 2):
-                self.flip_pebbles()
-            
-            # Append move to board and change turns
-            if (bool(re.match(r'[0-7][0-7]$', self.move)) and (self.board[
-                                int(self.move[0])][int(self.move[1])
-                                ] == 0)) and (bool(self.flip) == True):
-                self.board[int(self.move[0])][int(self.move[1])] = self.turn
-                self.print_board()
-                self.turn = ((self.turn % 2) + 1) # Changes turns
-            elif (bool(re.match(r'[0-7][0-7]$', self.move)) == True) and (
-                                            bool(self.flip_pebbles())):
+            match = (bool(re.match(r'[0-7][0-7]$', self.move)))
+            if (len(self.move) == 2) and match and (self.board[int(
+                                self.move[0])][int(self.move[1])] != 0):
                 print "Invalid: Player {} has a piece there.".format(
-                        self.board[int(self.move[0])][int(self.move[1])])
+                            self.board[int(self.move[0])][int(self.move[1])])
                 self.move = '99'
+            elif (len(self.move) == 2) and match and (self.board[int(
+                                self.move[0])][int(self.move[1])] == 0):
+                self.flip_pebbles()
+                if bool(self.flip) == True: 
+                    self.print_board() # Print the board
+                    self.turn = ((self.turn % 2) + 1) # Changes turns
+                else:
+                    print "INVALID: no flipped tiles. Try again"
+                    self.move = '99'
             else:
                 print "Invalid entry, try again"
+    
+    def run_reversi(self):
+        """This will run prompt_move() and new_game() and end_game() and 
+        potentially every function. This function should be the one that 
+        combines all of them until the game ends. This function
+        should bring together all the rules into one, so the user can call
+        just this one and this one only.  
+        """
+        pass
 
 
 game = Reversi()        #temporary for developing code
